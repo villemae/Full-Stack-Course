fdaca9def48988f134d80d96ccb73faaf6afb140
Ville <ville.maatta@nokia.com>
2022-06-10 11:41:05 +0300

    Part 2 exercises done

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Contacts, FilterForm } from './components/Contacts'
 import AddForm from './components/AddForm'
+import Notification from './components/Notification'
 import phonebookService from './services/persons'
 
 const App = () => {
@@ -8,6 +9,8 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState(false)
   
   useEffect(() => {
     console.log("effect")
@@ -19,9 +22,21 @@ const App = () => {
       })
   }, [])
 
+  const showNotification = () => {
+    setTimeout(() => {
+      setMessage(null)
+      setError(false)
+    }, 3000)
+  }
 
   const handleAddForm = (event) => {
     event.preventDefault()
+    if (newName === '' || newNumber === '') {
+      const notification = 'Fill all the fields!'
+      setMessage(notification)
+      showNotification()
+      return
+    }
     const confirmMsg = `${newName} is already in the phonebook,do you want to 
     replace the old number with a new one?`
     let contactObject = persons.find(person => person.name === newName)
@@ -34,7 +49,10 @@ const App = () => {
       phonebookService
       .create(contactObject)
       .then(response => {
+        const notification = `${newName} added successfully!`
         setPersons(persons.concat(response.data))
+        setMessage(notification)
+        showNotification()
       })
     }
     //if contact is already on the phonebook, update it
@@ -43,7 +61,16 @@ const App = () => {
       phonebookService
       .update(updatedContact)
       .then(response => {
+        const notification = `${updatedContact.name}: updated number succesfully`
+        setMessage(notification)
+        showNotification()
         setPersons(persons.map(person => person.name !== newName ? person : response.data))
+      })
+      .catch(() => {
+        const notification = `Error! Update ${updatedContact.name}'s number failed`
+        setError(true)
+        setMessage(notification)
+        showNotification()
       })
     }
     setNewName('')
@@ -56,9 +83,18 @@ const App = () => {
       phonebookService
       .del(contactToDelete.id)
       .then(response => {
+        const notification = `${contactToDelete.name} deleted successfully!`
+        setMessage(notification)
+        showNotification()
         setPersons(persons.filter(p => p.id !== id))
+
       })
-      .catch(error => console.log("ERROR! DELETE NOT SUCCESSFUL"))
+      .catch(() => {
+        const notification = `Error! Delete ${contactToDelete.name} failed`
+        setError(true)
+        setMessage(notification)
+        showNotification()
+      })
     }
   }
 
@@ -78,6 +114,7 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
+      <Notification message={message} error={error}/>
       <AddForm
         handleName={handleNameChange}
         handleNumber={handleNumberChange}
